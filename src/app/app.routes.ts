a54cf65035d3bc0c49fca41c20ee2ed27436ae69
Rebecca Marinelli <rebeccamarinelli18@gmail.com>
2024-11-03 11:46:26 +0100

    wrong page create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FavouritesComponent } from './pages/favourites/favourites.component';
+import { WrongRouteComponent } from './pages/wrong-route/wrong-route.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'favorites', component: FavouritesComponent }
+    {path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'favorites', component: FavouritesComponent },
+    {path : '**', component: WrongRouteComponent}
 ];
---
+++ src/app/pages/wrong-route/wrong-route.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-wrong-route',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './wrong-route.component.html',
+  styleUrl: './wrong-route.component.scss'
+})
+export class WrongRouteComponent {
+
+}
